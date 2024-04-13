a82d4110b1985213ee6fe5668bb072d63a9e52d9
snipes <schneidsmc@gmail.com>
2024-04-12 21:07:14 -0500

    fixed manifest json

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -34,8 +34,8 @@ module.exports = () => {
         description: 'Store your notes here!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
